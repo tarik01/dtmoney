75dfebda3c44551c52434fd483e330f33001dbd3
Tarik Nazorek <jantarik@gmail.com>
2021-09-27 22:35:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import {GlobalStyle} from "./styles/global";
+import Modal from "react-modal"
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
